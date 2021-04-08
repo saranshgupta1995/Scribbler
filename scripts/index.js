295c3aa6c17d9ae986f4cbeac1beacb1f7d5409d
Saransh Gupta <saranshgupta1995@gmail.com>
2021-04-08 07:16:43 +0530

    have the base in place

---
+++ scripts/index.js
@@ -0,0 +1,82 @@
+const FORMS = {
+  signup: [
+    {
+      title: "Get Started",
+      type: "title"
+    },
+    {
+      type: "text",
+      label: "Username",
+      placeholder: "Enter your username"
+    },
+    {
+      type: "password",
+      label: "Password",
+      placeholder: "Enter your password"
+    },
+    {
+      type: "password",
+      label: "Confirm Password",
+      placeholder: "Re-enter your password"
+    },
+    {
+      type: "submit",
+      text: "Signup"
+    }
+  ]
+};
+
+const removeModal = () => {};
+
+const createModal = id => {
+  const modalBg = createElem("div", {
+    class: "modal-bg",
+    id: `${id}-modal`
+  });
+  const modal = createElem("div", {
+    class: "modal"
+  });
+  document.body.addChildren(modalBg.addChildren(modal));
+  return modal;
+};
+
+const renderFormInModal = type => {
+  const formElements = FORMS[type];
+
+  const form = createElem("form").addChildren(
+    formElements
+      .map(elem => {
+        let domRepresentation = null;
+
+        switch (elem.type) {
+          case "title":
+            domRepresentation = createElem("div", {
+              class: "title"
+            }).addChildren([
+              createElem(
+                "h2",
+                {},
+                {},
+                {
+                  innerText: elem.title
+                }
+              ),
+              createElem("i", {
+                class: "fa fa-times"
+              })
+            ]);
+            break;
+
+          default:
+            break;
+        }
+
+        return domRepresentation;
+      })
+      .filter(x => !!x)
+  );
+
+  createModal(type).addChildren(form);
+};
+
+renderFormInModal("signup");
